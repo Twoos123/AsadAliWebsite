77a26a4ebaabf31faef50425d80bb15541a02832
Asad Ali <68927600+Twoos123@users.noreply.github.com>
2023-01-06 17:33:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const tl = gsap.timeline({defaults: {duration: 0.75, ease: "power1.out" }})
 
-tl.fromTo('.cookie-container', {scale: 0}, {scale: 1, ease: "elastic.out(1, 0.4)", duration: 1.5})
+tl.fromTo('.text', {scale: 0}, {scale: 1, ease: "elastic.out(1, 0.4)", duration: 1.5})
 
 //tl.fromTo('.cookie-container', {scale: 0}, {scale: 1, ease: "elastic.out(1, 0.4)", duration: 1.5}) // whole popup pops in from inside screen
 //tl.fromTo('.cookie', {opacity: 0, x:-50, rotation: '-45deg'}, {opacity: 1, x:0, rotation: '0deg'}, '<50%') // runs first animation, then this one

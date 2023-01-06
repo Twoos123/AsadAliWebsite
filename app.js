43692c9fd8f400216033160e3d98276729ab17cc
Asad Ali <68927600+Twoos123@users.noreply.github.com>
2023-01-06 17:29:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry)
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show);
-    }
-  });
-});
+const tl = gsap.timeline({defaults: {duration: 0.75, ease: "power1.out" }})
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+tl.fromTo('.cookie-container', {scale: 0}, {scale: 1, ease: "elastic.out(1, 0.4)", duration: 1.5}) // whole popup pops in from inside screen
+tl.fromTo('.cookie', {opacity: 0, x:-50, rotation: '-45deg'}, {opacity: 1, x:0, rotation: '0deg'}, '<50%') // runs first animation, then this one
+tl.fromTo('.header', {x: 30, opacity: 0}, {x: 0, opacity: 1}) // '<' syncs up animation timing w the previous animation, very useful
+// <50% makes it so that it syncs the animation halfway 
 
+//Cookie Jump
+tl.fromTo('.cookie', {y: 0, rotation: '0deg'}, {y: -20, yoyo: true, repeat: -1, rotation: '-20deg'}) //-1 on repeat makes it loop indefinitely
+tl.fromTo('#crumbs', {y: 0}, {y: -20, yoyo: true, repeat: -1}, '<') //-1 on repeat makes it loop indefinitely
+
+//Fading the cookie out
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    gsap.to('.cookie-container', {opacity: 0, y: 100, duration: 0.75, ease: "power1.out"})
+})

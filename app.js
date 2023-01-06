2f05c52d9a8b317d7dc3f125f9aeddbb1488afd6
Asad Ali <68927600+Twoos123@users.noreply.github.com>
2023-01-06 17:43:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const tl = gsap.timeline({defaults: {duration: 0.75, ease: "power1.out" }})
 
 tl.fromTo('.text', {scale: 0}, {scale: 1, ease: "elastic.out(1, 0.4)", duration: 1.5})
+t1.fromTo('.header', {opacity: 0, x:-50, rotation: '-45deg'}, {opacity: 1, x:0, rotation: '0deg'}, '<50%')
 
 //tl.fromTo('.cookie-container', {scale: 0}, {scale: 1, ease: "elastic.out(1, 0.4)", duration: 1.5}) // whole popup pops in from inside screen
 //tl.fromTo('.cookie', {opacity: 0, x:-50, rotation: '-45deg'}, {opacity: 1, x:0, rotation: '0deg'}, '<50%') // runs first animation, then this one
@@ -11,7 +12,7 @@ tl.fromTo('.text', {scale: 0}, {scale: 1, ease: "elastic.out(1, 0.4)", duration:
 //tl.fromTo('.cookie', {y: 0, rotation: '0deg'}, {y: -20, yoyo: true, repeat: -1, rotation: '-20deg'}) //-1 on repeat makes it loop indefinitely
 //tl.fromTo('#crumbs', {y: 0}, {y: -20, yoyo: true, repeat: -1}, '<') //-1 on repeat makes it loop indefinitely
 
-//Fading the cookie out
+//Fading the text out (if there is a button)
 //const button = document.querySelector('button');
 //button.addEventListener('click', () => {
    // gsap.to('.cookie-container', {opacity: 0, y: 100, duration: 0.75, ease: "power1.out"})
